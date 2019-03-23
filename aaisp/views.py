d0c92a61c1cb52c2c9b645729954fadf4e7418c8
Daniel Cordero <aa-bw@0xdc.io>
2019-03-23 14:35:56 +0000

    Defer fetching new bandwidth usage for each line

--- aaisp/views.py
+++ aaisp/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from djangae.deferred import defer
 from .models import Line
+
+def _update_line(line_pk):
+    line = Line.objects.get(pk=line_pk)
+    line.save_new()
 
 def update_lines(self, request=None):
     for line in Line.objects.all():
-        line.save_new()
+        defer(_update_line, line.pk)
     return HttpResponse("OK")
