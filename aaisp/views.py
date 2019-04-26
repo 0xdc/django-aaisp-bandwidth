99afbef6393b332a9e0a95a86c39473949ff12f7
Daniel Cordero <aa-bw@0xdc.io>
2019-04-26 08:17:53 +0000

    Add login_required to the update view to prevent abuse

--- aaisp/views.py
+++ aaisp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Line
 
+@login_required
 def update_lines(self, request=None):
     for line in Line.objects.all():
         line.save_new()
